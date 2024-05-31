74e0b87265d8cccbbe0ac3fd9ae3c6af3c79add7
Developersemja <developer.semja@gmail.com>
2024-05-31 11:56:06 +0530

    test-p

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1 // Adjust threshold if necessary
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('.text-animation').forEach(element => {
+        observer.observe(element);
+    });
+});
