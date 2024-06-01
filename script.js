733c140f5c0bced7e2690abe4279fda521a4154d
Developersemja <developer.semja@gmail.com>
2024-06-01 18:05:07 +0530

    a

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
 //--------------------------------Text Animation-----------------------------//
 
-document.addEventListener('DOMContentLoaded', () => {
-    const observerOptions = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 0.1 // Adjust threshold if necessary
-    };
+document.addEventListener("DOMContentLoaded", () => {
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1, 
+  };
 
-    const observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('animate');
-                observer.unobserve(entry.target);
-            }
-        });
-    }, observerOptions);
-
-    document.querySelectorAll('.text-animation').forEach(element => {
-        observer.observe(element);
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+        observer.unobserve(entry.target);
+      }
     });
+  }, observerOptions);
+
+  document.querySelectorAll(".text-animation").forEach((element) => {
+    observer.observe(element);
+  });
 });
 
-
 //--------------------------------Lottie-----------------------------//
+document.addEventListener("DOMContentLoaded", () => {
+  const lottiePlayer = document.getElementById("lottie");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const lottiePlayer = document.getElementById('lottie');
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           lottiePlayer.play();
         } else {
           lottiePlayer.stop();
         }
       });
-    }, {
-      threshold: 1.0
-    });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
 
-    observer.observe(lottiePlayer);
-  });
-
-
-  
+  observer.observe(lottiePlayer);
+});
